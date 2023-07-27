434640ce8f786bdecdc9db569073bd0be21f3188
makinot1 <makinot1@gene.com>
2023-07-26 18:17:21 -0700

    Global RNG for each data module

--- domain_invariant_generation_toy/data/colored_mnist.py
+++ domain_invariant_generation_toy/data/colored_mnist.py
@@ -5,18 +5,18 @@ from torchvision import datasets
 from utils.nn_utils import make_dataloader
 
 
+RNG = np.random.RandomState(0)
 PROB_ZERO_E0 = 0.25
 
 
-def flip_binary(rng, x, flip_prob):
+def flip_binary(x, flip_prob):
     idxs = np.arange(len(x))
-    flip_idxs = rng.choice(idxs, size=int(flip_prob * len(idxs)), replace=False)
+    flip_idxs = RNG.choice(idxs, size=int(flip_prob * len(idxs)), replace=False)
     x[flip_idxs] = 1 - x[flip_idxs]
     return x
 
 
 def make_raw_data():
-    rng = np.random.RandomState()
     mnist = datasets.MNIST(os.environ['DATA_DPATH'], train=True, download=True)
     binary_idxs = np.where(mnist.targets <= 1)
     images, digits = mnist.data[binary_idxs], mnist.targets[binary_idxs]
@@ -25,25 +25,25 @@ def make_raw_data():
     idxs_env0 = []
     zero_idxs = np.where(digits == 0)[0]
     one_idxs = np.where(digits == 1)[0]
-    idxs_env0.append(rng.choice(zero_idxs, size=int(PROB_ZERO_E0 * len(zero_idxs))))
-    idxs_env0.append(rng.choice(one_idxs, size=int((1 - PROB_ZERO_E0) * len(one_idxs))))
+    idxs_env0.append(RNG.choice(zero_idxs, size=int(PROB_ZERO_E0 * len(zero_idxs))))
+    idxs_env0.append(RNG.choice(one_idxs, size=int((1 - PROB_ZERO_E0) * len(one_idxs))))
     idxs_env0 = np.concatenate(idxs_env0)
     idxs_env1 = np.setdiff1d(np.arange(n_total), idxs_env0)
 
     e = torch.zeros(n_total)
     e[idxs_env1] = 1
 
-    y = flip_binary(rng, digits.clone(), 0.25)
+    y = flip_binary(digits.clone(), 0.25)
 
     colors = np.full(n_total, np.nan)
     idxs_y0_e0 = np.where((y == 0) & (e == 0))[0]
     idxs_y0_e1 = np.where((y == 0) & (e == 1))[0]
     idxs_y1_e0 = np.where((y == 1) & (e == 0))[0]
     idxs_y1_e1 = np.where((y == 1) & (e == 1))[0]
-    colors[idxs_y0_e0] = rng.normal(0.2, 0.01, len(idxs_y0_e0))
-    colors[idxs_y0_e1] = rng.normal(0.4, 0.01, len(idxs_y0_e1))
-    colors[idxs_y1_e0] = rng.normal(0.6, 0.01, len(idxs_y1_e0))
-    colors[idxs_y1_e1] = rng.normal(0.8, 0.01, len(idxs_y1_e1))
+    colors[idxs_y0_e0] = RNG.normal(0.2, 0.01, len(idxs_y0_e0))
+    colors[idxs_y0_e1] = RNG.normal(0.4, 0.01, len(idxs_y0_e1))
+    colors[idxs_y1_e0] = RNG.normal(0.6, 0.01, len(idxs_y1_e0))
+    colors[idxs_y1_e1] = RNG.normal(0.8, 0.01, len(idxs_y1_e1))
     colors = np.clip(colors, 0, 1)[:, None, None]
 
     images = torch.stack([images, images], dim=1)
@@ -57,11 +57,10 @@ def make_raw_data():
 
 
 def make_data(train_ratio, batch_size, n_workers):
-    rng = np.random.RandomState()
     e, digits, y, colors, x = make_raw_data()
     n_total = len(e)
     n_train = int(train_ratio * n_total)
-    train_idxs = rng.choice(np.arange(n_total), n_train, replace=False)
+    train_idxs = RNG.choice(np.arange(n_total), n_train, replace=False)
     val_idxs = np.setdiff1d(np.arange(n_total), train_idxs)
     x_train, y_train, e_train = x[train_idxs], y[train_idxs], e[train_idxs]
     x_val, y_val, e_val = x[val_idxs], y[val_idxs], e[val_idxs]
--- domain_invariant_generation_toy/data/dsprites.py
+++ domain_invariant_generation_toy/data/dsprites.py
@@ -1,11 +1,10 @@
 import numpy as np
-import os
-import pandas as pd
 import torch
 from utils.nn_utils import make_dataloader
 from utils.stats import min_max_scale
 
 
+RNG = np.random.RandomState(0)
 N_TOTAL = 10000
 WIDTH_LB = 8
 WIDTH_UB = 32
@@ -13,37 +12,34 @@ IMAGE_SIZE = 128
 
 
 def make_raw_data():
-    rng = np.random.RandomState()
-    idxs_env0 = rng.choice(np.arange(N_TOTAL), N_TOTAL // 2, replace=False)
+    idxs_env0 = RNG.choice(np.arange(N_TOTAL), N_TOTAL // 2, replace=False)
     idxs_env1 = np.setdiff1d(np.arange(N_TOTAL), idxs_env0)
 
     e = np.zeros(N_TOTAL)
     e[idxs_env1] = 1
 
     width = np.full(N_TOTAL, np.nan)
-    width[idxs_env0] = (rng.normal(16, 3, len(idxs_env0))).astype(int)
-    width[idxs_env1] = (rng.normal(24, 3, len(idxs_env1))).astype(int)
+    width[idxs_env0] = (RNG.normal(18, 3, len(idxs_env0))).astype(int)
+    width[idxs_env1] = (RNG.normal(22, 3, len(idxs_env1))).astype(int)
     width = width + width % 2
     width = np.clip(width, WIDTH_LB, WIDTH_UB)
 
-    y = width ** 2
-    y = min_max_scale(y)
-
-    y = rng.binomial(1, y, len(y))
+    y = min_max_scale(width)
+    y = RNG.binomial(1, y, len(y))
 
     brightness = np.full(N_TOTAL, np.nan)
     idxs_y0_e0 = np.where((y == 0) & (e == 0))[0]
     idxs_y0_e1 = np.where((y == 0) & (e == 1))[0]
     idxs_y1_e0 = np.where((y == 1) & (e == 0))[0]
     idxs_y1_e1 = np.where((y == 1) & (e == 1))[0]
-    brightness[idxs_y0_e0] = rng.normal(0.3, 0.01, len(idxs_y0_e0))
-    brightness[idxs_y0_e1] = rng.normal(0.7, 0.01, len(idxs_y0_e1))
-    brightness[idxs_y1_e0] = rng.normal(0.5, 0.01, len(idxs_y1_e0))
-    brightness[idxs_y1_e1] = rng.normal(0.9, 0.01, len(idxs_y1_e1))
+    brightness[idxs_y0_e0] = RNG.normal(0.3, 0.01, len(idxs_y0_e0))
+    brightness[idxs_y0_e1] = RNG.normal(0.7, 0.01, len(idxs_y0_e1))
+    brightness[idxs_y1_e0] = RNG.normal(0.5, 0.01, len(idxs_y1_e0))
+    brightness[idxs_y1_e1] = RNG.normal(0.9, 0.01, len(idxs_y1_e1))
     brightness = np.clip(brightness, 0, 1)[:, None]
 
-    center_x = rng.randint(WIDTH_UB // 2, IMAGE_SIZE - WIDTH_UB // 2 + 1, N_TOTAL)
-    center_y = rng.randint(WIDTH_UB // 2, IMAGE_SIZE - WIDTH_UB // 2 + 1, N_TOTAL)
+    center_x = RNG.randint(WIDTH_UB // 2, IMAGE_SIZE - WIDTH_UB // 2 + 1, N_TOTAL)
+    center_y = RNG.randint(WIDTH_UB // 2, IMAGE_SIZE - WIDTH_UB // 2 + 1, N_TOTAL)
 
     x = np.zeros((N_TOTAL, IMAGE_SIZE, IMAGE_SIZE))
     for idx in range(N_TOTAL):
@@ -60,11 +56,10 @@ def make_raw_data():
 
 
 def make_data(train_ratio, batch_size, n_workers):
-    rng = np.random.RandomState()
-    e, scale, y, brightness, x = make_raw_data()
+    e, width, y, brightness, x = make_raw_data()
     n_total = len(x)
     n_train = int(train_ratio * n_total)
-    train_idxs = rng.choice(np.arange(n_total), n_train, replace=False)
+    train_idxs = RNG.choice(np.arange(n_total), n_train, replace=False)
     val_idxs = np.setdiff1d(np.arange(n_total), train_idxs)
     x_train, y_train, e_train = x[train_idxs], y[train_idxs], e[train_idxs]
     x_val, y_val, e_val = x[val_idxs], y[val_idxs], e[val_idxs]
