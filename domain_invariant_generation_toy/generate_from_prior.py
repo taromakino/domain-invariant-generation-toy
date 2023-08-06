c40ec9ca5a1e896438fa8a5d3d1d093837dde145
Taro Makino <taro@nyu.edu>
2023-08-06 18:29:26 -0400

    Refactor generate_from_prior

--- domain_invariant_generation_toy/generate_from_prior.py
+++ domain_invariant_generation_toy/generate_from_prior.py
@@ -21,36 +21,36 @@ def main(args):
     existing_args = load_file(os.path.join(args.dpath, f'version_{args.seed}', 'args.pkl'))
     pl.seed_everything(existing_args.seed)
     data_train, _, _ = MAKE_DATA[existing_args.dataset](existing_args.train_ratio, existing_args.batch_size)
-    vae = VAE.load_from_checkpoint(os.path.join(args.dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'),
-        map_location='cpu')
+    vae = VAE.load_from_checkpoint(os.path.join(args.dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'))
     x_train, y_train, e_train = data_train.dataset[:]
+    x_train, y_train, e_train = x_train.to(vae.device), y_train.to(vae.device), e_train.to(vae.device)
     y_idx_train = y_train.int()[:, 0]
     e_idx_train = e_train.int()[:, 0]
-    x_seed, y_idx_seed, e_idx_seed = x_train[[args.example_idx]], y_idx_train[[args.example_idx]], \
-        e_idx_train[[args.example_idx]]
-    posterior_dist_seed = vae.posterior_dist(x_seed, y_idx_seed, e_idx_seed)
-    z_seed = posterior_dist_seed.loc
-    zc_seed, zs_seed = torch.chunk(z_seed, 2, dim=1)
-    fig, axes = plt.subplots(2, args.n_cols, figsize=(2 * args.n_cols, 2 * 2))
-    for ax in axes.flatten():
-        ax.set_xticks([])
-        ax.set_yticks([])
-    plot = PLOT[existing_args.dataset]
-    image_size = IMAGE_SHAPE[existing_args.dataset]
-    plot(axes[0, 0], x_seed.reshape(image_size).detach().numpy())
-    plot(axes[1, 0], x_seed.reshape(image_size).detach().numpy())
-    x_pred = torch.sigmoid(vae.decoder(z_seed))
-    plot(axes[0, 1], x_pred.reshape(image_size).detach().numpy())
-    plot(axes[1, 1], x_pred.reshape(image_size).detach().numpy())
-    for col_idx in range(2, args.n_cols):
-        zc_sample, zs_sample = sample_prior(rng, vae, y_idx_train, e_idx_train)
-        x_pred_causal = torch.sigmoid(vae.decoder(torch.hstack((zc_sample, zs_seed))))
-        x_pred_spurious = torch.sigmoid(vae.decoder(torch.hstack((zc_seed, zs_sample))))
-        plot(axes[0, col_idx], x_pred_causal.reshape(image_size).detach().numpy())
-        plot(axes[1, col_idx], x_pred_spurious.reshape(image_size).detach().numpy())
-    fig_dpath = os.path.join(args.dpath, f'version_{args.seed}', 'fig', 'generate_from_prior')
-    os.makedirs(fig_dpath, exist_ok=True)
-    plt.savefig(os.path.join(fig_dpath, f'{args.example_idx}.png'))
+    for example_idx in range(args.n_examples):
+        x_seed, y_idx_seed, e_idx_seed = x_train[[example_idx]], y_idx_train[[example_idx]], e_idx_train[[example_idx]]
+        posterior_dist_seed = vae.posterior_dist(x_seed, y_idx_seed, e_idx_seed)
+        z_seed = posterior_dist_seed.loc
+        zc_seed, zs_seed = torch.chunk(z_seed, 2, dim=1)
+        fig, axes = plt.subplots(2, args.n_cols, figsize=(2 * args.n_cols, 2 * 2))
+        for ax in axes.flatten():
+            ax.set_xticks([])
+            ax.set_yticks([])
+        plot = PLOT[existing_args.dataset]
+        image_size = IMAGE_SHAPE[existing_args.dataset]
+        plot(axes[0, 0], x_seed.reshape(image_size).detach().cpu().numpy())
+        plot(axes[1, 0], x_seed.reshape(image_size).detach().cpu().numpy())
+        x_pred = torch.sigmoid(vae.decoder(z_seed))
+        plot(axes[0, 1], x_pred.reshape(image_size).detach().cpu().numpy())
+        plot(axes[1, 1], x_pred.reshape(image_size).detach().cpu().numpy())
+        for col_idx in range(2, args.n_cols):
+            zc_sample, zs_sample = sample_prior(rng, vae, y_idx_train, e_idx_train)
+            x_pred_causal = torch.sigmoid(vae.decoder(torch.hstack((zc_sample, zs_seed))))
+            x_pred_spurious = torch.sigmoid(vae.decoder(torch.hstack((zc_seed, zs_sample))))
+            plot(axes[0, col_idx], x_pred_causal.reshape(image_size).detach().cpu().numpy())
+            plot(axes[1, col_idx], x_pred_spurious.reshape(image_size).detach().cpu().numpy())
+        fig_dpath = os.path.join(args.dpath, f'version_{args.seed}', 'fig', 'generate_from_prior')
+        os.makedirs(fig_dpath, exist_ok=True)
+        plt.savefig(os.path.join(fig_dpath, f'{example_idx}.png'))
 
 
 if __name__ == '__main__':
@@ -58,5 +58,5 @@ if __name__ == '__main__':
     parser.add_argument('--dpath', type=str, required=True)
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--n_cols', type=int, default=10)
-    parser.add_argument('--example_idx', type=int, default=0)
+    parser.add_argument('--n_examples', type=int, default=10)
     main(parser.parse_args())
