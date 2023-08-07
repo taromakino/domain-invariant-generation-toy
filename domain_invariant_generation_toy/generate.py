172f4dbf45f2b74be757573a9aac9d4766648599
Taro Makino <taro@nyu.edu>
2023-08-07 09:34:12 -0400

    Generate on GPU

--- domain_invariant_generation_toy/generate.py
+++ domain_invariant_generation_toy/generate.py
@@ -21,40 +21,41 @@ def main(args):
     existing_args = load_file(os.path.join(args.dpath, f'version_{args.seed}', 'args.pkl'))
     pl.seed_everything(existing_args.seed)
     data_train, _, _ = MAKE_DATA[existing_args.dataset](existing_args.train_ratio, existing_args.batch_size)
-    vae = VAE.load_from_checkpoint(os.path.join(args.dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'),
-        map_location='cpu')
+    vae = VAE.load_from_checkpoint(os.path.join(args.dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'))
     vae.freeze()
-    x, y, e = data_train.dataset[:]
-    y_idx = y.int()[:, 0]
-    e_idx = e.int()[:, 0]
-    posterior_dist = vae.posterior_dist(x, y_idx, e_idx)
+    x_train, y_train, e_train = data_train.dataset[:]
+    x_train, y_train, e_train = x_train.to(vae.device), y_train.to(vae.device), e_train.to(vae.device)
+    y_idx_train = y_train.int()[:, 0]
+    e_idx_train = e_train.int()[:, 0]
+    posterior_dist = vae.posterior_dist(x_train, y_idx_train, e_idx_train)
     z = posterior_dist.loc
     zc, zs = torch.chunk(z, 2, dim=1)
     p_zc = multivariate_normal(zc)
-    x_seed, y_seed, z_seed = x[[args.example_idx]], y[[args.example_idx]], z[[args.example_idx]]
-    fig, axes = plt.subplots(1, args.n_cols, figsize=(2 * args.n_cols, 2))
-    for ax in axes.flatten():
-        ax.set_xticks([])
-        ax.set_yticks([])
-    plot = PLOT[existing_args.dataset]
-    image_size = IMAGE_SHAPE[existing_args.dataset]
-    plot(axes[0], x_seed.reshape(image_size).detach().numpy())
-    x_pred = torch.sigmoid(vae.decoder(z_seed))
-    plot(axes[1], x_pred.reshape(image_size).detach().numpy())
-    zc_seed, zs_seed = torch.chunk(z_seed, 2, dim=1)
-    zc_perturb = zc_seed.clone().requires_grad_()
-    optimizer = Adam([zc_perturb], lr=args.lr)
-    for col_idx in range(2, args.n_cols):
-        for _ in range(args.n_steps_per_col):
-            optimizer.zero_grad()
-            loss = -log_prob_yzc(vae, p_zc, 1 - y_seed, zc_perturb)
-            loss.backward()
-            optimizer.step()
-        x_perturb = torch.sigmoid(vae.decoder(torch.hstack((zc_perturb, zs_seed))))
-        plot(axes[col_idx], x_perturb.reshape(image_size).detach().numpy())
-    fig_dpath = os.path.join(args.dpath, f'version_{args.seed}', 'fig', 'generate')
-    os.makedirs(fig_dpath, exist_ok=True)
-    plt.savefig(os.path.join(fig_dpath, f'{args.example_idx}.png'))
+    for example_idx in range(args.n_examples):
+        x_seed, y_seed, z_seed = x_train[[example_idx]], y_train[[example_idx]], z[[example_idx]]
+        fig, axes = plt.subplots(1, args.n_cols, figsize=(2 * args.n_cols, 2))
+        for ax in axes.flatten():
+            ax.set_xticks([])
+            ax.set_yticks([])
+        plot = PLOT[existing_args.dataset]
+        image_size = IMAGE_SHAPE[existing_args.dataset]
+        plot(axes[0], x_seed.reshape(image_size).detach().cpu().numpy())
+        x_pred = torch.sigmoid(vae.decoder(z_seed))
+        plot(axes[1], x_pred.reshape(image_size).detach().cpu().numpy())
+        zc_seed, zs_seed = torch.chunk(z_seed, 2, dim=1)
+        zc_perturb = zc_seed.clone().requires_grad_()
+        optimizer = Adam([zc_perturb], lr=args.lr)
+        for col_idx in range(2, args.n_cols):
+            for _ in range(args.n_steps_per_col):
+                optimizer.zero_grad()
+                loss = -log_prob_yzc(vae, p_zc, 1 - y_seed, zc_perturb)
+                loss.backward()
+                optimizer.step()
+            x_perturb = torch.sigmoid(vae.decoder(torch.hstack((zc_perturb, zs_seed))))
+            plot(axes[col_idx], x_perturb.reshape(image_size).detach().cpu().numpy())
+        fig_dpath = os.path.join(args.dpath, f'version_{args.seed}', 'fig', 'generate')
+        os.makedirs(fig_dpath, exist_ok=True)
+        plt.savefig(os.path.join(fig_dpath, f'{example_idx}.png'))
 
 
 if __name__ == '__main__':
@@ -63,6 +64,6 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--n_cols', type=int, default=10)
     parser.add_argument('--n_steps_per_col', type=int, default=5000)
-    parser.add_argument('--example_idx', type=int, default=0)
+    parser.add_argument('--n_examples', type=int, default=10)
     parser.add_argument('--lr', type=float, default=0.01)
     main(parser.parse_args())
