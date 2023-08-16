3c2f7b7b68cd70e18a9784d7d8223412c0793f46
makinot1 <makinot1@gene.com>
2023-08-16 14:37:50 -0400

    python_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='domain_invariant_generation_toy',
     packages=find_packages(),
+    python_requires='3.11',
     install_requires=[
         'matplotlib',
         'pandas',
