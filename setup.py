82aa421cef5f279b255cb044c73b897d4ae9e8f1
makinot1 <makinot1@gene.com>
2023-08-16 14:47:23 -0400

    Remove python_requires

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-
+# Python 3.11
 setup(
     name='domain_invariant_generation_toy',
     packages=find_packages(),
-    python_requires='3.11',
     install_requires=[
         'matplotlib',
         'pandas',
