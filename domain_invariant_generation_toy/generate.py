da69dc9ad1fd1420423f9a7416fbcfca54d919b9
makinot1 <makinot1@gene.com>
2023-08-31 09:20:34 -0400

    y_mult in generate

--- domain_invariant_generation_toy/generate.py
+++ domain_invariant_generation_toy/generate.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import os
 import pytorch_lightning as pl
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from argparse import ArgumentParser
 from data import MAKE_DATA, PLOT, IMAGE_SHAPE
@@ -10,18 +11,20 @@ from utils.file import load_file
 from models.vae import VAE
 
 
-def log_prob_yzc(vae, y, z_c):
+def log_prob_yzc(y, z_c, vae, q_causal):
     y_pred = vae.causal_classifier(z_c)
     log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y)
-    log_prob_zc = vae.q(z_c).mean()
-    return log_prob_y_zc + log_prob_zc
+    log_prob_zc = q_causal.log_prob(z_c).mean()
+    return log_prob_y_zc, log_prob_zc
 
 
 def main(args):
     existing_args = load_file(os.path.join(args.dpath, f'version_{args.seed}', 'args.pkl'))
     pl.seed_everything(existing_args.seed)
     data_train, _, _ = MAKE_DATA[existing_args.dataset](existing_args.train_ratio, existing_args.batch_size)
-    vae = VAE.load_from_checkpoint(os.path.join(args.dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'))
+    vae = VAE.load_from_checkpoint(os.path.join(args.dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'),
+        stage='test')
+    q_causal = vae.q_causal()
     x_train, y_train, e_train = data_train.dataset[:]
     x_train, y_train, e_train = x_train.to(vae.device), y_train.to(vae.device), e_train.to(vae.device)
     posterior_dist = vae.encoder(x_train, y_train, e_train)
@@ -38,12 +41,13 @@ def main(args):
         x_pred = torch.sigmoid(vae.decoder.mlp(z_seed))
         plot(axes[1], x_pred.reshape(image_size).detach().cpu().numpy())
         zc_seed, zs_seed = torch.chunk(z_seed, 2, dim=1)
-        zc_param = zc_seed.clone().requires_grad_()
+        zc_param = nn.Parameter(zc_seed)
         optimizer = Adam([zc_param], lr=args.lr_generate)
         for col_idx in range(2, args.n_cols):
             for _ in range(args.n_steps_per_col):
                 optimizer.zero_grad()
-                loss = -log_prob_yzc(vae, 1 - y_seed, zc_param)
+                log_prob_y_zc, log_prob_zc = log_prob_yzc(1 - y_seed, zc_param, vae, q_causal)
+                loss = args.y_mult * -log_prob_y_zc - log_prob_zc
                 loss.backward()
                 optimizer.step()
             x_perturb = torch.sigmoid(vae.decoder.mlp(torch.hstack((zc_param, zs_seed))))
@@ -57,9 +61,9 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--dpath', type=str, required=True)
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--lr_generate', type=float, default=1e-3)
-    parser.add_argument('--n_steps_per_col', type=int, default=5000)
+    parser.add_argument('--y_mult', type=float, default=1000)
+    parser.add_argument('--lr_generate', type=float, default=0.01)
+    parser.add_argument('--n_steps_per_col', type=int, default=1000)
     parser.add_argument('--n_cols', type=int, default=10)
     parser.add_argument('--n_examples', type=int, default=10)
-
     main(parser.parse_args())
