f717f8b7f3ee327659ccb9751c74b576f413bb43
makinot1 <makinot1@gene.com>
2023-09-01 16:35:17 -0400

    Update dsprites script

--- scripts/dsprites.sh
+++ scripts/dsprites.sh
@@ -1,29 +1,32 @@
 export CODE_DPATH=/home/makinot1/git/domain-invariant-generation-toy/domain_invariant_generation_toy
+export DPATH=$RESULTS_DPATH/dsprites
 
 python $CODE_DPATH/main.py \
 --dataset dsprites \
---dpath $RESULTS_DPATH/dsprites/train_vae \
---stage train_vae \
---prior_reg_mult 0.1 \
---q_mult 1
+--dpath $DPATH \
+--task train_vae
 
 python $CODE_DPATH/main.py \
 --dataset dsprites \
---dpath $RESULTS_DPATH/dsprites/train_classifier \
---ckpt_fpath $RESULTS_DPATH/dsprites/train_vae/version_0/checkpoints/best.ckpt \
---stage train_classifier
+--dpath $DPATH \
+--task train_q
 
 python $CODE_DPATH/main.py \
 --dataset dsprites \
---dpath $RESULTS_DPATH/dsprites/train_q \
---ckpt_fpath $RESULTS_DPATH/dsprites/train_classifier/version_0/checkpoints/best.ckpt \
---stage train_q
+--dpath $DPATH \
+--task infer_z_train
 
 python $CODE_DPATH/main.py \
 --dataset dsprites \
---dpath $RESULTS_DPATH/dsprites/test \
---ckpt_fpath $RESULTS_DPATH/dsprites/train_q/version_0/checkpoints/best.ckpt \
---stage test
+--dpath $DPATH \
+--task infer_z_val
 
-python $CODE_DPATH/generate_from_prior.py \
---dpath $RESULTS_DPATH/dsprites/train_vae
+python $CODE_DPATH/main.py \
+--dataset dsprites \
+--dpath $DPATH \
+--task infer_z_test
+
+python $CODE_DPATH/main.py \
+--dataset dsprites \
+--dpath $DPATH \
+--task classify
