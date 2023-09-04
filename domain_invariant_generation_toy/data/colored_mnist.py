26275bf53a6c39aad01f9be7862e604f655759f3
makinot1 <makinot1@gene.com>
2023-09-04 13:31:40 -0400

    Make color less predictive

--- domain_invariant_generation_toy/data/colored_mnist.py
+++ domain_invariant_generation_toy/data/colored_mnist.py
@@ -43,8 +43,8 @@ def make_trainval_data():
     idxs_y1_e0 = np.where((y == 1) & (e == 0))[0]
     idxs_y1_e1 = np.where((y == 1) & (e == 1))[0]
     colors[idxs_y0_e0] = RNG.normal(0.2, 0.05, len(idxs_y0_e0))
-    colors[idxs_y0_e1] = RNG.normal(0.45, 0.05, len(idxs_y0_e1))
-    colors[idxs_y1_e0] = RNG.normal(0.55, 0.05, len(idxs_y1_e0))
+    colors[idxs_y0_e1] = RNG.normal(0.6, 0.05, len(idxs_y0_e1))
+    colors[idxs_y1_e0] = RNG.normal(0.4, 0.05, len(idxs_y1_e0))
     colors[idxs_y1_e1] = RNG.normal(0.8, 0.05, len(idxs_y1_e1))
     colors = np.clip(colors, 0, 1)[:, None, None]
 
