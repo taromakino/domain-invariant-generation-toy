29cfe3723174cc8ddc8815ecb0360b264a544969
makinot1 <makinot1@gene.com>
2023-09-09 15:52:18 -0400

    Update scripts

--- scripts/dsprites.sh
+++ scripts/dsprites.sh
@@ -6,18 +6,17 @@ export DPATH=$RESULTS_DPATH/$DATASET
 python $CODE_DPATH/main.py \
 --dataset $DATASET \
 --dpath $DPATH \
---task train_vae
+--task vae
 
 python $CODE_DPATH/main.py \
 --dataset $DATASET \
 --dpath $DPATH \
---task train_classifier
+--task inference_encoder
 
 python $CODE_DPATH/main.py \
 --dataset $DATASET \
 --dpath $DPATH \
---task inference \
---batch_size 2048
+--task inference
 
 python $CODE_DPATH/generate_sample_prior.py \
 --dpath $DPATH
