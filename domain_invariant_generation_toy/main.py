3b7e6f517241649fbf4b8f6df77ad0af011ba333
makinot1 <makinot1@gene.com>
2023-09-13 17:55:57 -0400

    Beta-VAE

--- domain_invariant_generation_toy/main.py
+++ domain_invariant_generation_toy/main.py
@@ -33,8 +33,8 @@ def main(args):
         trainer.fit(model, data_train, data_val)
         trainer.test(model, data_test, ckpt_path='best')
     elif args.task == Task.VAE:
-        model = Model(task_dpath, args.seed, args.task, X_SIZE[args.dataset], args.z_size, args.h_sizes, args.weight_decay,
-            args.lr, args.lr_inference, args.n_steps)
+        model = Model(task_dpath, args.seed, args.task, X_SIZE[args.dataset], args.z_size, args.h_sizes, args.beta,
+            args.weight_decay, args.lr, args.lr_inference, args.n_steps)
         trainer = make_trainer(task_dpath, args.seed, args.n_epochs, args.early_stop_ratio, True)
         trainer.fit(model, data_train, data_val)
     elif args.task == Task.Q_Z:
@@ -70,6 +70,7 @@ if __name__ == '__main__':
     parser.add_argument('--is_erm', action='store_true')
     parser.add_argument('--z_size', type=int, default=50)
     parser.add_argument('--h_sizes', nargs='+', type=int, default=[512, 512])
+    parser.add_argument('--beta', type=float, default=1)
     parser.add_argument('--weight_decay', type=float, default=1e-5)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--lr_inference', type=float, default=0.01)
