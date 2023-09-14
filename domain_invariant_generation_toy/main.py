52f6f33d7adc20332bb67cb3a741a3e28d86099e
makinot1 <makinot1@gene.com>
2023-09-14 11:46:13 -0400

    Minor changes

--- domain_invariant_generation_toy/main.py
+++ domain_invariant_generation_toy/main.py
@@ -12,50 +12,53 @@ from utils.file import save_file
 from utils.nn_utils import make_dataloader
 
 
-def make_trainer(task_dpath, seed, n_epochs, early_stop_ratio, inference_mode):
-    return pl.Trainer(
-        logger=CSVLogger(task_dpath, name='', version=seed),
-        callbacks=[
-            EarlyStopping(monitor='val_loss', patience=int(early_stop_ratio * n_epochs)),
-            ModelCheckpoint(monitor='val_loss', filename='best')],
-        max_epochs=n_epochs,
-        inference_mode=inference_mode)
-
-
 def main(args):
+    def make_trainer(task_dpath, n_epochs, inference_mode):
+        return pl.Trainer(
+            logger=CSVLogger(task_dpath, name='', version=args.seed),
+            callbacks=[
+                EarlyStopping(monitor='val_loss', patience=int(args.early_stop_ratio * args.n_epochs)),
+                ModelCheckpoint(monitor='val_loss', filename='best')],
+            max_epochs=n_epochs,
+            inference_mode=inference_mode)
+
+
+    def ckpt_fpath(inference_stage):
+        return os.path.join(args.dpath, args.z_norm_mult_str, inference_stage.value, f'version_{args.seed}',
+            'checkpoints', 'best.ckpt')
+
+
     pl.seed_everything(args.seed)
-    task_dpath = os.path.join(args.dpath, f'z_norm_mult={args.z_norm_mult}', args.task.value)
+    z_norm_mult_str = f'z_norm_mult={args.z_norm_mult}'
+    task_dpath = os.path.join(args.dpath, z_norm_mult_str, args.task.value)
     save_file(args, os.path.join(task_dpath, f'version_{args.seed}', 'args.pkl'))
     data_train, data_val, data_test = MAKE_DATA[args.dataset](args.train_ratio, args.batch_size)
     if args.task == Task.ERM_Y_C or args.task == Task.ERM_Y_S or args.task == Task.ERM_Y_X:
         model = ERM(args.task, X_SIZE[args.dataset], args.h_sizes, args.lr)
-        trainer = make_trainer(task_dpath, args.seed, args.n_epochs, args.early_stop_ratio, True)
+        trainer = make_trainer(task_dpath, args.n_epochs, True)
         trainer.fit(model, data_train, data_val)
         trainer.test(model, data_test, ckpt_path='best')
     elif args.task == Task.VAE:
         model = Model(task_dpath, args.seed, args.task, X_SIZE[args.dataset], args.z_size, args.h_sizes, args.z_norm_mult,
             args.weight_decay, args.lr, args.lr_inference, args.n_steps)
-        trainer = make_trainer(task_dpath, args.seed, args.n_epochs, args.early_stop_ratio, True)
+        trainer = make_trainer(task_dpath, args.n_epochs, True)
         trainer.fit(model, data_train, data_val)
     elif args.task == Task.Q_Z:
-        ckpt_fpath = os.path.join(args.dpath, Task.VAE.value, f'version_{args.seed}', 'checkpoints', 'best.ckpt')
-        model = Model.load_from_checkpoint(ckpt_fpath, task=args.task)
-        trainer = make_trainer(task_dpath, args.seed, 1, args.early_stop_ratio, True)
+        model = Model.load_from_checkpoint(ckpt_fpath(Task.VAE), task=args.task)
+        trainer = make_trainer(task_dpath, 1, True)
         trainer.test(model, data_train)
         trainer.save_checkpoint(os.path.join(task_dpath, f'version_{args.seed}', 'checkpoints', 'best.ckpt'))
     elif args.task == Task.INFER_Z:
-        ckpt_fpath = os.path.join(args.dpath, Task.Q_Z.value, f'version_{args.seed}', 'checkpoints', 'best.ckpt')
-        model = Model.load_from_checkpoint(ckpt_fpath, dpath=task_dpath, task=args.task, lr_inference=args.lr_inference,
-            n_steps=args.n_steps)
-        trainer = make_trainer(task_dpath, args.seed, args.n_epochs, args.early_stop_ratio, False)
+        model = Model.load_from_checkpoint(ckpt_fpath(Task.Q_Z), dpath=task_dpath, task=args.task,
+            lr_inference=args.lr_inference, n_steps=args.n_steps)
+        trainer = make_trainer(task_dpath, args.n_epochs, False)
         trainer.test(model, data_test)
     else:
         assert args.task == Task.CLASSIFY
-        data_test = make_dataloader(torch.load(os.path.join(args.dpath, Task.INFER_Z.value, f'version_{args.seed}',
-            'z.pt')), args.batch_size, False)
-        ckpt_fpath = os.path.join(args.dpath, Task.Q_Z.value, f'version_{args.seed}', 'checkpoints', 'best.ckpt')
-        model = Model.load_from_checkpoint(ckpt_fpath, task=args.task)
-        trainer = make_trainer(task_dpath, args.seed, args.n_epochs, args.early_stop_ratio, True)
+        data_test = make_dataloader(torch.load(os.path.join(args.dpath, z_norm_mult_str, Task.INFER_Z.value,
+            f'version_{args.seed}', 'z.pt')), args.batch_size, False)
+        model = Model.load_from_checkpoint(ckpt_fpath(Task.Q_Z), task=args.task)
+        trainer = make_trainer(task_dpath, 1, True)
         trainer.test(model, data_test)
 
 
--- scripts/dsprites.sh
+++ scripts/dsprites.sh
@@ -3,10 +3,13 @@ export CODE_DPATH=/home/makinot1/git/domain-invariant-generation-toy/domain_inva
 export DATASET=dsprites
 export DPATH=$RESULTS_DPATH/$DATASET
 
+export Z_NORM_MULT=0.001
+
 python $CODE_DPATH/main.py \
 --dataset $DATASET \
 --dpath $DPATH \
---task vae
+--task vae \
+--z_norm_mult $Z_NORM_MULT
 
 python $CODE_DPATH/main.py \
 --dataset $DATASET \
