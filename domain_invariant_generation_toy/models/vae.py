2a64bcf782455c524690ce5453d5d54e2decc070
makinot1 <makinot1@gene.com>
2023-09-29 11:16:18 -0400

    Minor changes

--- domain_invariant_generation_toy/models/vae.py
+++ domain_invariant_generation_toy/models/vae.py
@@ -15,16 +15,16 @@ class Encoder(nn.Module):
         super().__init__()
         self.z_size = z_size
         self.rank = rank
-        self.mu = MLP(x_size, h_sizes, 2 * z_size)
-        self.low_rank = MLP(x_size, h_sizes, 2 * z_size * rank)
-        self.diag = MLP(x_size, h_sizes, 2 * z_size)
+        self.mu = MLP(x_size + N_CLASSES + N_ENVS, h_sizes, 2 * z_size)
+        self.low_rank = MLP(x_size + N_CLASSES + N_ENVS, h_sizes, 2 * z_size * rank)
+        self.diag = MLP(x_size + N_CLASSES + N_ENVS, h_sizes, 2 * z_size)
 
-    def forward(self, x):
+    def forward(self, x, y_embed, e_embed):
         batch_size = len(x)
-        mu = self.mu(x)
-        low_rank = self.low_rank(x)
+        mu = self.mu(x, y_embed, e_embed)
+        low_rank = self.low_rank(x, y_embed, e_embed)
         low_rank = low_rank.reshape(batch_size, 2 * self.z_size, self.rank)
-        diag = self.diag(x)
+        diag = self.diag(x, y_embed, e_embed)
         return D.MultivariateNormal(mu, scale_tril=arr_to_tril(low_rank, diag))
 
 
@@ -39,52 +39,56 @@ class Decoder(nn.Module):
 
 
 class Prior(nn.Module):
-    def __init__(self, z_size, rank):
+    def __init__(self, z_size, rank, h_sizes):
         super().__init__()
         self.z_size = z_size
-        self.mu_causal = nn.Parameter(torch.zeros(N_ENVS, z_size))
-        self.low_rank_causal = nn.Parameter(torch.zeros(N_ENVS, z_size, rank))
-        self.diag_causal = nn.Parameter(torch.zeros(N_ENVS, z_size))
-        nn.init.kaiming_normal_(self.mu_causal)
-        nn.init.kaiming_normal_(self.low_rank_causal)
-        nn.init.kaiming_normal_(self.diag_causal)
+        self.rank = rank
+        # p(z_c|e)
+        self.mu_causal = MLP(N_CLASSES, h_sizes, z_size)
+        self.low_rank_causal = MLP(N_CLASSES, h_sizes, z_size * rank)
+        self.diag_causal = MLP(N_CLASSES, h_sizes, z_size)
         # p(z_s|y,e)
-        self.mu_spurious = nn.Parameter(torch.zeros(N_CLASSES, N_ENVS, z_size))
-        self.low_rank_spurious = nn.Parameter(torch.zeros(N_CLASSES, N_ENVS, z_size, rank))
-        self.diag_spurious = nn.Parameter(torch.zeros(N_CLASSES, N_ENVS, z_size))
-        nn.init.kaiming_normal_(self.mu_spurious)
-        nn.init.kaiming_normal_(self.low_rank_spurious)
-        nn.init.kaiming_normal_(self.diag_spurious)
+        self.mu_spurious = MLP(N_CLASSES + N_ENVS, h_sizes, z_size)
+        self.low_rank_spurious = MLP(N_CLASSES + N_ENVS, h_sizes, z_size * rank)
+        self.diag_spurious = MLP(N_CLASSES + N_ENVS, h_sizes, z_size)
 
-    def forward(self, y, e):
-        batch_size = len(y)
-        mu_causal = self.mu_causal[e]
-        mu_spurious = self.mu_spurious[y, e]
+    def forward(self, y_embed, e_embed):
+        batch_size = len(y_embed)
+        mu_causal = self.mu_causal(y_embed)
+        low_rank_causal = self.low_rank_causal(y_embed)
+        low_rank_causal = low_rank_causal.reshape(batch_size, self.z_size, self.rank)
+        diag_causal = self.diag_causal(y_embed)
+        mu_spurious = self.mu_spurious(y_embed, e_embed)
+        low_rank_spurious = self.low_rank_spurious(y_embed, e_embed)
+        low_rank_spurious = low_rank_spurious.reshape(batch_size, self.z_size, self.rank)
+        diag_spurious = self.diag_spurious(y_embed, e_embed)
         mu = torch.hstack((mu_causal, mu_spurious))
-        cov_causal = arr_to_cov(self.low_rank_causal[e], self.diag_causal[e])
-        cov_spurious = arr_to_cov(self.low_rank_spurious[y, e], self.diag_spurious[y, e])
-        cov = torch.zeros(batch_size, 2 * self.z_size, 2 * self.z_size, device=y.device)
+        cov_causal = arr_to_cov(low_rank_causal, diag_causal)
+        cov_spurious = arr_to_cov(low_rank_spurious, diag_spurious)
+        cov = torch.zeros(batch_size, 2 * self.z_size, 2 * self.z_size, device=y_embed.device)
         cov[:, :self.z_size, :self.z_size] = cov_causal
         cov[:, self.z_size:, self.z_size:] = cov_spurious
         return D.MultivariateNormal(mu, cov)
 
 
 class VAE(pl.LightningModule):
-    def __init__(self, task, x_size, z_size, rank, h_sizes, y_mult, reg_mult, weight_decay, lr):
+    def __init__(self, task, x_size, z_size, rank, h_sizes, beta, reg_mult, weight_decay, lr):
         super().__init__()
         self.save_hyperparameters()
         self.task = task
         self.z_size = z_size
-        self.y_mult = y_mult
+        self.beta = beta
         self.reg_mult = reg_mult
         self.weight_decay = weight_decay
         self.lr = lr
+        self.y_embed = nn.Embedding(N_CLASSES, N_CLASSES)
+        self.e_embed = nn.Embedding(N_ENVS, N_ENVS)
         # q(z_c|x,y,e)
         self.encoder = Encoder(x_size, z_size, rank, h_sizes)
         # p(x|z_c,z_s)
         self.decoder = Decoder(x_size, z_size, h_sizes)
         # p(z_c,z_s|y,e)
-        self.prior = Prior(z_size, rank)
+        self.prior = Prior(z_size, rank, h_sizes)
         # p(y|z_c)
         self.classifier = MLP(z_size, h_sizes, 1)
         # q(z)
@@ -103,8 +107,10 @@ class VAE(pl.LightningModule):
         return D.MultivariateNormal(self.q_mu, covariance_matrix=torch.diag(self.q_var))
 
     def vae_loss(self, x, y, e):
+        y_embed = self.y_embed(y)
+        e_embed = self.e_embed(e)
         # z_c,z_s ~ q(z_c,z_s|x,y,e)
-        posterior_dist = self.encoder(x)
+        posterior_dist = self.encoder(x, y_embed, e_embed)
         z = self.sample_z(posterior_dist)
         # E_q(z_c,z_s|x,y,e)[log p(x|z_c,z_s)]
         log_prob_x_z = self.decoder(x, z).mean()
@@ -113,7 +119,7 @@ class VAE(pl.LightningModule):
         y_pred = self.classifier(z_c.detach()).view(-1)
         log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y.float())
         # KL(q(z_c,z_s|x) || p(z_c|e)p(z_s|y,e))
-        prior_dist = self.prior(y, e)
+        prior_dist = self.prior(y_embed, e_embed)
         kl = D.kl_divergence(posterior_dist, prior_dist).mean()
         z_norm = (z ** 2).mean()
         return log_prob_x_z, log_prob_y_zc, kl, z_norm
@@ -121,29 +127,70 @@ class VAE(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y, e, c, s = batch
         log_prob_x_z, log_prob_y_zc, kl, z_norm = self.vae_loss(x, y, e)
-        loss = -log_prob_x_z - self.y_mult * log_prob_y_zc + kl + self.reg_mult * z_norm
+        loss = -log_prob_x_z - log_prob_y_zc + self.beta * kl + self.reg_mult * z_norm
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y, e, c, s = batch
         log_prob_x_z, log_prob_y_zc, kl, z_norm = self.vae_loss(x, y, e)
-        loss = -log_prob_x_z - self.y_mult * log_prob_y_zc + kl + self.reg_mult * z_norm
+        loss = -log_prob_x_z - log_prob_y_zc + self.beta * kl + self.reg_mult * z_norm
         self.log('val_log_prob_x_z', log_prob_x_z, on_step=False, on_epoch=True)
         self.log('val_log_prob_y_zc', log_prob_y_zc, on_step=False, on_epoch=True)
         self.log('val_kl', kl, on_step=False, on_epoch=True)
         self.log('val_z_norm', z_norm, on_step=False, on_epoch=True)
         self.log('val_loss', loss, on_step=False, on_epoch=True)
 
+    def e_invariant_loss(self, x, z, q):
+        log_prob_x_z = self.decoder(x, z).mean()
+        z_c, z_s = torch.chunk(z, 2, dim=1)
+        y_pred = self.classifier(z_c)
+        prob_y_pos_zc = torch.sigmoid(y_pred)
+        prob_y_neg_zc = 1 - prob_y_pos_zc
+        prob_y_zc = torch.hstack((prob_y_neg_zc, prob_y_pos_zc))
+        log_prob_y_zc = torch.log(prob_y_zc.max(dim=1).values).mean()
+        log_prob_z = q.log_prob(z).mean()
+        return log_prob_x_z, log_prob_y_zc, log_prob_z
+
+    def infer_z(self, x):
+        batch_size = len(x)
+        q = self.q()
+        z_sample = q.sample((batch_size,))
+        z_param = nn.Parameter(z_sample)
+        optim = Adam([z_param], lr=self.lr_infer)
+        optim_loss = torch.inf
+        optim_log_prob_x_z = optim_log_prob_y_zc = optim_log_prob_z = optim_z = None
+        for _ in range(self.n_infer_steps):
+            optim.zero_grad()
+            log_prob_x_z, log_prob_y_zc, log_prob_z = self.e_invariant_loss(x, z_param, q)
+            loss = -log_prob_x_z - log_prob_y_zc - log_prob_z
+            loss.backward()
+            optim.step()
+            if loss < optim_loss:
+                optim_loss = loss
+                optim_log_prob_x_z = log_prob_x_z
+                optim_log_prob_y_zc = log_prob_y_zc
+                optim_log_prob_z = log_prob_z
+                optim_z = z_param.clone()
+        return optim_z, optim_log_prob_x_z, optim_log_prob_y_zc, optim_log_prob_z, optim_loss
+
     def test_step(self, batch, batch_idx):
         x, y, e, c, s = batch
         if self.task == Task.Q_Z:
-            z = self.encoder(x).loc
+            y_embed = self.y_embed(y)
+            e_embed = self.e_embed(e)
+            z = self.encoder(x, y_embed, e_embed).loc
             self.z_sample.append(z.detach().cpu())
         else:
-            z = self.encoder(x).loc
-            self.z_infer.append(z.detach().cpu())
-            self.y.append(y.cpu())
-            self.e.append(e.cpu())
+            assert self.task == Task.INFER_Z
+            with torch.set_grad_enabled(True):
+                z, log_prob_x_z, log_prob_y_zc, log_prob_z, loss = self.infer_z(x)
+                self.log('log_prob_x_z', log_prob_x_z, on_step=False, on_epoch=True)
+                self.log('log_prob_y_zc', log_prob_y_zc, on_step=False, on_epoch=True)
+                self.log('log_prob_z', log_prob_z, on_step=False, on_epoch=True)
+                self.log('loss', loss, on_step=False, on_epoch=True)
+                self.z_infer.append(z.detach().cpu())
+                self.y.append(y.cpu())
+                self.e.append(e.cpu())
 
     def on_test_epoch_end(self):
         if self.task == Task.Q_Z:
