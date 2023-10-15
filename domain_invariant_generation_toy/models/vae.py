a395a09c425911ab2ce0430b06b0b81a92879b4a
makinot1 <makinot1@gene.com>
2023-10-15 12:07:18 -0400

    classifier_mult, prior_mult

--- domain_invariant_generation_toy/models/vae.py
+++ domain_invariant_generation_toy/models/vae.py
@@ -103,12 +103,14 @@ class Prior(nn.Module):
 
 
 class VAE(pl.LightningModule):
-    def __init__(self, task, x_size, z_size, rank, h_sizes, x_mult, reg_mult, lr, weight_decay, lr_infer, n_infer_steps):
+    def __init__(self, task, x_size, z_size, rank, h_sizes, classifier_mult, prior_mult, reg_mult, lr, weight_decay,
+            lr_infer, n_infer_steps):
         super().__init__()
         self.save_hyperparameters()
         self.task = task
         self.z_size = z_size
-        self.x_mult = x_mult
+        self.classifier_mult = classifier_mult
+        self.prior_mult = prior_mult
         self.reg_mult = reg_mult
         self.lr = lr
         self.weight_decay = weight_decay
@@ -150,25 +152,28 @@ class VAE(pl.LightningModule):
         log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y.float())
         # KL(q(z_c,z_s|x) || p(z_c|e)p(z_s|y,e))
         prior_dist = self.prior(y, e)
-        kl = D.kl_divergence(posterior_dist, prior_dist).mean()
+        log_prob_z_ye = prior_dist.log_prob(z).mean()
+        entropy = posterior_dist.entropy().mean()
         prior_norm = (prior_dist.loc ** 2).mean()
-        return self.x_mult * log_prob_x_z, log_prob_y_zc, kl, self.reg_mult * prior_norm
+        return log_prob_x_z, self.classifier_mult * log_prob_y_zc, self.prior_mult * log_prob_z_ye, entropy, \
+            self.reg_mult * prior_norm
 
     def training_step(self, batch, batch_idx):
         assert self.task == Task.VAE
         x, y, e, c, s = batch
-        log_prob_x_z, log_prob_y_zc, kl, prior_norm = self.loss(x, y, e)
-        loss = -log_prob_x_z - log_prob_y_zc + kl + prior_norm
+        log_prob_x_z, log_prob_y_zc, log_prob_z_ye, entropy, prior_norm = self.loss(x, y, e)
+        loss = -log_prob_x_z - log_prob_y_zc - log_prob_z_ye - entropy + prior_norm
         return loss
 
     def validation_step(self, batch, batch_idx):
         assert self.task == Task.VAE
         x, y, e, c, s = batch
-        log_prob_x_z, log_prob_y_zc, kl, prior_norm = self.loss(x, y, e)
-        loss = -log_prob_x_z - log_prob_y_zc + kl + prior_norm
+        log_prob_x_z, log_prob_y_zc, log_prob_z_ye, entropy, prior_norm = self.loss(x, y, e)
+        loss = -log_prob_x_z - log_prob_y_zc - log_prob_z_ye - entropy + prior_norm
         self.log('val_log_prob_x_z', log_prob_x_z, on_step=False, on_epoch=True)
         self.log('val_log_prob_y_zc', log_prob_y_zc, on_step=False, on_epoch=True)
-        self.log('val_kl', kl, on_step=False, on_epoch=True)
+        self.log('val_log_prob_z_ye', log_prob_z_ye, on_step=False, on_epoch=True)
+        self.log('val_entropy', entropy, on_step=False, on_epoch=True)
         self.log('val_prior_norm', prior_norm, on_step=False, on_epoch=True)
         self.log('val_loss', loss, on_step=False, on_epoch=True)
 
@@ -186,7 +191,7 @@ class VAE(pl.LightningModule):
         for y_elem in range(N_CLASSES):
             y = torch.full((batch_size,), y_elem, dtype=torch.long, device=self.device)
             log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y.float(), reduction='none')
-            loss_candidates.append((self.x_mult * -log_prob_x_z - log_prob_y_zc - log_prob_z)[:, None])
+            loss_candidates.append((-log_prob_x_z - self.classifier_mult * log_prob_y_zc - log_prob_z)[:, None])
             y_candidates.append(y_elem)
         loss_candidates = torch.hstack(loss_candidates).min(dim=1)
         y_candidates = torch.tensor(y_candidates, device=self.device)
